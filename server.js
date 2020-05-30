d716ef3dcb03e8432f8bf6ceb7ba97f74c51b5b0
Jordan <jlewis2008@live.com>
2020-05-29 20:53:21 -0400

    initial commit, DB setup

---
+++ server.js
@@ -0,0 +1,82 @@
+"use strict";
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var expect = require("chai").expect;
+var cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+var apiRoutes = require("./routes/api.js");
+var fccTestingRoutes = require("./routes/fcctesting.js");
+var runner = require("./test-runner");
+
+dotenv.config({ path: "./.env" });
+
+var app = express();
+
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Sample front-end
+app.route("/b/:board/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/board.html");
+});
+app.route("/b/:board/:threadid").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/thread.html");
+});
+
+//Index page (static HTML)
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+//For FCC testing purposes
+fccTestingRoutes(app);
+
+//Routing for API
+apiRoutes(app);
+
+//Sample Front-end
+
+//404 Not Found Middleware
+app.use(function (req, res, next) {
+  res.status(404).type("text").send("Not Found");
+});
+
+const DB = process.env.DB.replace("<password>", process.env.PASSWORD);
+
+//Start our server and tests!
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.clear();
+    console.log("DB Connected!");
+    app.listen(process.env.PORT || 3000, function () {
+      console.log("Listening on port " + process.env.PORT);
+      if (process.env.NODE_ENV === "test") {
+        console.log("Running Tests...");
+        setTimeout(function () {
+          try {
+            runner.run();
+          } catch (e) {
+            var error = e;
+            console.log("Tests are not valid:");
+            console.log(error);
+          }
+        }, 1500);
+      }
+    });
+  })
+  .catch((err) => console.log(err));
+
+module.exports = app; //for testing
