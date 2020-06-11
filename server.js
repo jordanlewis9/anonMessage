51815e14ef17be741a2e06a5944725f1ab3046ae
Jordan <jlewis2008@live.com>
2020-06-10 20:38:04 -0400

    completed backend

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ dotenv.config({ path: "./.env" });
 var app = express();
 
 app.use(helmet());
+app.use(helmet.referrerPolicy({ policy: "same-origin" }));
 
 app.use("/public", express.static(process.cwd() + "/public"));
 
